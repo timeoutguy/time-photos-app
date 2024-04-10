6cb484e2453b3425ad67bbc9801c4c6c0c64c6ac
Marcus Vinícius <marcus.silva0001@totvs.com.br>
2024-04-10 09:36:22 -0300

    feat: navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { octUpload } from '@ng-icons/octicons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  providers: [provideIcons({ octUpload })],
 })
 export class NavbarComponent {
 
