b7d030f85571763b21394c4a9a3fa16a32f9d770
Marcus Vinícius <marcus.dev@protonmail.com>
2024-04-18 21:02:12 -0300

    feat: perfoming logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { octMoon, octSun, octUpload } from '@ng-icons/octicons';
 import { UserSignalsStateService } from '../../services/store/user/user-signals-state.service';
+import { SessionService } from '../../services/session.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-navbar',
@@ -13,7 +15,18 @@ import { UserSignalsStateService } from '../../services/store/user/user-signals-
   providers: [provideIcons({ octUpload, octMoon, octSun })],
 })
 export class NavbarComponent {
-  private userState = inject(UserSignalsStateService);
+  readonly userState = inject(UserSignalsStateService);
+  readonly sessionService = inject(SessionService);
+  readonly cookieService = inject(CookieService);
+  readonly router = inject(Router);
 
   user = this.userState.select('user');
+
+  public logout() {
+    this.sessionService.logout().subscribe(() => {
+      this.userState.clear();
+      this.cookieService.delete('token');
+      this.router.navigate(['']);
+    });
+  }
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild, inject } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { octChevronDown } from '@ng-icons/octicons';
 import { ImageCardComponent } from '../../image-card/image-card.component';
+import { ImageService } from '../../services/image.service';
+import { UserSignalsStateService } from '../../services/store/user/user-signals-state.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,12 +13,19 @@ import { ImageCardComponent } from '../../image-card/image-card.component';
   styleUrl: './dashboard.component.scss',
   providers: [provideIcons({ octChevronDown})]
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   @ViewChild("searchInput") searchInput!: ElementRef;
 
   @HostListener('document:keydown.control.k', ['$event'])
   openModal(event: KeyboardEvent) {
     event.preventDefault();
     this.searchInput.nativeElement.focus();
+  }
+
+  private imageService = inject(ImageService);
+  private userState = inject(UserSignalsStateService);
+
+  ngOnInit(): void {
+    this.imageService.getImages().subscribe(result => console.log(result));
   }
 }
